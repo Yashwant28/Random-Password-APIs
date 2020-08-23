1de6d0ce99ea4b3a60f1c7bed5faa02446760440
Yashwant28 <jangid.yashwant28@gmail.com>
2020-08-24 03:22:08 +0530

    Added JUnits For JerseyConfig file

--- src/test/java/com/stark/tech/config/JerseyConfigTest.java
+++ src/test/java/com/stark/tech/config/JerseyConfigTest.java
@@ -1,5 +1,21 @@
 package com.stark.tech.config;
 
+import com.stark.tech.resource.HealthCheck;
+import com.stark.tech.resource.RandomPasswordGeneratorResource;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@RunWith(MockitoJUnitRunner.class)
 public class JerseyConfigTest {
+
+    @Test
+    public void testJerseyConfig() {
+        JerseyConfig jerseyConfig = new JerseyConfig();
+        assertTrue(jerseyConfig.isRegistered(HealthCheck.class));
+        assertTrue(jerseyConfig.isRegistered(RandomPasswordGeneratorResource.class));
+    }
 
 }

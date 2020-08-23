c1b13908e3eee927b5c33fd13df2fd7ebcd6d414
Yashwant28 <jangid.yashwant28@gmail.com>
2020-08-24 03:40:43 +0530

    Added test For Application.java to make coverage 100 percent

--- src/test/java/com/stark/tech/ApplicationTest.java
+++ src/test/java/com/stark/tech/ApplicationTest.java
@@ -1,4 +1,33 @@
 package com.stark.tech;
 
+import com.stark.tech.model.StatusMessage;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.ws.rs.core.Response;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class ApplicationTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void hello() {
+        ResponseEntity<String> entity = this.restTemplate.getForEntity("/healthCheck",
+                String.class);
+        assertEquals(entity.getStatusCode(), HttpStatus.OK);
+        assertEquals("{\"message\":\"Health Check Works!!\"}", entity.getBody());
+    }
+
 }
